de4010362aa9c7bf9a6d54bc44bf3546592aca01
cctv1237 <lf@LFdeMacBook-Pro.local>
2015-04-02 18:58:56 +0800

    Initial Commit

---
+++ ScanList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScanList
+//
+//  Created by LF on 15/4/2.
+//  Copyright (c) 2015年 FAN LONG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ScanList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScanList
+//
+//  Created by LF on 15/4/2.
+//  Copyright (c) 2015年 FAN LONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
